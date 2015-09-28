4c729feb683b7be7a68126572216427f94f2526f
ba5bx <ba5bx@virginia.edu>
2015-09-28 14:34:18 -0400

    test_file hello: add()

--- hello.py
+++ hello.py
@@ -2,4 +2,8 @@ __author__ = "BrianAguirre"
 
 from helper import greeting
 
+def add(x, y):
+	total = x+y
+	print (str(total))
+
 greeting("Hello")
---
+++ test_files.py
@@ -0,0 +1,6 @@
+from helper import greeting 
+from hello import add
+
+greeting("Test test test baby!")
+add(4, 4)
+
